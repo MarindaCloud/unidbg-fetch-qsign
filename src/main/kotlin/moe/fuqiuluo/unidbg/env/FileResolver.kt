3dd85c4daa33a0580c5e56c26fcdc9ce530e4c8c
FQL <ydic001@vip.qq.com>
2023-07-16 11:03:42 +0800

    fix trying to fetch boot_id

--- src/main/kotlin/moe/fuqiuluo/unidbg/env/FileResolver.kt
+++ src/main/kotlin/moe/fuqiuluo/unidbg/env/FileResolver.kt
@@ -13,6 +13,7 @@ import moe.fuqiuluo.unidbg.QSecVM
 import moe.fuqiuluo.unidbg.env.files.fetchStat
 import moe.fuqiuluo.unidbg.env.files.fetchStatus
 import java.io.File
+import java.util.UUID
 import java.util.logging.Logger
 
 class FileResolver(
@@ -20,6 +21,7 @@ class FileResolver(
     vm: QSecVM
 ): AndroidResolver(sdk) {
     private val tmpFilePath = vm.coreLibPath
+    private val uuid = UUID.randomUUID()
 
     override fun resolve(emulator: Emulator<AndroidFileIO>, path: String, oflags: Int): FileResult<AndroidFileIO>? {
         val result = super.resolve(emulator, path, oflags)
@@ -37,6 +39,9 @@ class FileResolver(
         }
 
 
+        if (path == "/proc/sys/kernel/random/boot_id") {
+            return FileResult.success(ByteArrayFileIO(oflags, path, uuid.toString().toByteArray()))
+        }
         if (path == "/proc/self/status") {
             return FileResult.success(ByteArrayFileIO(oflags, path, fetchStatus(emulator.pid).toByteArray()))
         }
