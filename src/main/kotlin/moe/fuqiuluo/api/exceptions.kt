836b92d87b4e9ee045c98b859ea0b07c68619a83
FQL <ydic001@vip.qq.com>
2023-07-18 10:55:33 +0800

    support blacklist

--- src/main/kotlin/moe/fuqiuluo/api/exceptions.kt
+++ src/main/kotlin/moe/fuqiuluo/api/exceptions.kt
@@ -7,3 +7,5 @@ object SessionNotFoundError: RuntimeException("Uin is not registered.")
 object WrongKeyError: RuntimeException("Wrong API key.")
 
 object MissingKeyError: RuntimeException("First use must be submitted with android_id and guid.")
+
+object BlackListError: RuntimeException("Blacklist uin.")
