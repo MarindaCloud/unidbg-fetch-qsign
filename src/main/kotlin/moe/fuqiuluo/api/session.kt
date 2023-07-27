ca414eb030ff4c37112575c40a896889b833bae8
FQL <ydic001@vip.qq.com>
2023-07-27 16:33:57 +0800

    fix #164

--- src/main/kotlin/moe/fuqiuluo/api/session.kt
+++ src/main/kotlin/moe/fuqiuluo/api/session.kt
@@ -1,10 +1,15 @@
 package moe.fuqiuluo.api
 
 import CONFIG
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.sync.Mutex
 import kotlinx.coroutines.sync.withLock
 import moe.fuqiuluo.unidbg.session.Session
 import moe.fuqiuluo.unidbg.session.SessionManager
 import kotlin.concurrent.timer
+import kotlin.contracts.ExperimentalContracts
+import kotlin.contracts.InvocationKind
+import kotlin.contracts.contract
 
 fun initSession(uin: Long): Session? {
     return SessionManager[uin] ?: if (!CONFIG.autoRegister) {
@@ -18,11 +23,26 @@ fun findSession(uin: Long): Session {
     return SessionManager[uin] ?: throw SessionNotFoundError
 }
 
-internal suspend inline fun <T> Session.withLock(block: () -> T): T {
-    val job = timer(initialDelay = 5000, period = 5000) {
-        if (mutex.isLocked) mutex.unlock()
+internal suspend inline fun <T> Session.withLock(action: () -> T): T {
+    return mutex.withLockAndTimeout(5000, action)
+}
+
+@OptIn(ExperimentalContracts::class)
+private suspend inline fun <T> Mutex.withLockAndTimeout(timeout: Long, action: () -> T): T {
+    contract {
+        callsInPlace(action, InvocationKind.EXACTLY_ONCE)
     }
-    return mutex.withLock {
-        block().also { job.cancel() }
+
+    lock()
+    val job = timer(initialDelay = timeout, period = timeout) {
+        if (isLocked)
+            unlock()
+    }
+    try {
+        return action().also {
+            job.cancel()
+        }
+    } finally {
+        unlock()
     }
 }
